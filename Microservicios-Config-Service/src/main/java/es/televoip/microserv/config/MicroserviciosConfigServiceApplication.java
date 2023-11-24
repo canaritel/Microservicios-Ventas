1432eea9287798fb727fe6aa3c2e473b0b20c85f
Televoip <toni@televoip.es>
2023-11-24 11:21:13 +0000

    Proyecto Microservicios-Config-Service

--- Microservicios-Config-Service/src/main/java/es/televoip/microserv/config/MicroserviciosConfigServiceApplication.java
+++ Microservicios-Config-Service/src/main/java/es/televoip/microserv/config/MicroserviciosConfigServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class MicroserviciosCloudConfigApplication {
+public class MicroserviciosConfigServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviciosCloudConfigApplication.class, args);
+		SpringApplication.run(MicroserviciosConfigServiceApplication.class, args);
 	}
 
 }
